bdd4f054af6c3d17d725f138d2091509fe446899
Ph-FMM <pedroaraujo1952@hotmail.com>
2020-05-01 12:42:39 -0400

    Initialize backend using Node.Js

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ status: "Server is running" });
+});
+
+app.listen(3333, () => console.log("Backend running in port 3333"));
