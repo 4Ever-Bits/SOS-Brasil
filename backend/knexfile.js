a51226524d564ea16cee4d11ca89c8c090c11042
Ph-FMM <pedroaraujo1952@hotmail.com>
2020-05-09 20:49:54 -0400

    Update knex file

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -4,10 +4,10 @@ module.exports = {
   development: {
     client: "mysql",
     connection: {
-      host: "127.0.0.1",
-      user: "root",
-      password: "",
-      database: "sosbrasil",
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     migrations: {
       directory: "./src/database/migrations",
