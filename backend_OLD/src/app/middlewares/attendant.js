d822f5362ec370ea0f6d745853b3aa9ef7e214a7
Ph-FMM <pedroaraujo1952@hotmail.com>
2020-07-01 10:07:55 -0400

    Delete old backend

--- backend_OLD/src/app/middlewares/attendant.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  const { decodedToken } = req.body;
-
-  if (decodedToken.type !== "user") next();
-  else return res.status(401).json({ error: "Unauthorized" });
-};
